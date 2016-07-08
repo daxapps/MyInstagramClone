37ad791b0f68fe5af2a73385d68a5b9f8b4a3f06
Jason Crawford <daxapps777@gmail.com>
2016-07-08 09:03:19 -0500

    making progress

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -10,24 +10,89 @@
 import UIKit
 import Parse
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    
+    @IBAction func pause(sender: AnyObject) {
+        
+        activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0, 0, 50, 50))
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+        //UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+        
+    }
+    
+    @IBAction func restore(sender: AnyObject) {
+        
+        activityIndicator.stopAnimating()
+        //UIApplication.sharedApplication().endIgnoringInteractionEvents()
+    }
+    
+    @IBAction func createAlert(sender: AnyObject) {
+       
+        let alert = UIAlertController(title: "Hey there!", message: "Are you sure?", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+            
+        }))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        
+        print("Image Selected")
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        importedImage.image = image
+        
+    }
 
+    
+    @IBAction func importImage(sender: AnyObject) {
+        
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        image.allowsEditing = false
+        
+        self.presentViewController(image, animated: true, completion: nil)
+        
+    }
+    
+    @IBOutlet var importedImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var product = PFObject(className: "Products")
         
-        product["name"] = "Pizza"
         
-        product["description"] = "Deliciously cheesy"
         
-        product["price"] = 9.99
+        
+        
+        /*
+        let product = PFObject(className: "Products")
+        
+        product["name"] = "Ice Cream"
+        
+        product["description"] = "Tutti Fruiti"
+        
+        product["price"] = 4.99
         
         product.saveInBackgroundWithBlock { (success, error) -> Void in
             
             if success == true {
                 
-                print("Successful")
+                print ("Object saved with ID \(product.objectId)")
                 
             } else {
                 
@@ -36,8 +101,29 @@ class ViewController: UIViewController {
             }
         
         }
+
+        let query = PFQuery(className: "Products")
         
-        
+        query.getObjectInBackgroundWithId("Xy6KrJKO7D", block: { (object: PFObject?, error: NSError? ) -> Void in
+            
+            if error != nil {
+                
+                print(error)
+                
+            } else if let product = object {
+                
+                product["description"] = "Rocky Road"
+                
+                product["price"] = 5.99
+                
+                product.saveInBackground()
+                
+                //print(object!.objectForKey("description"))
+                
+            }
+            
+        })
+*/
     }
 
     override func didReceiveMemoryWarning() {
--- ParseStarterProject/test.swift
+++
@@ -1,9 +0,0 @@
-//
-//  test.swift
-//  ParseStarterProject-Swift
-//
-//  Created by Jason Crawford on 7/7/16.
-//  Copyright © 2016 Parse. All rights reserved.
-//
-
-import Foundation
